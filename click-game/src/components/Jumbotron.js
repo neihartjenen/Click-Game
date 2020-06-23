6c0b431c119014dbee1ba4949d0e03ea74d2c7fb
jenenneihart <neihart.jenen@gmail.com>
2020-06-22 22:34:50 -0700

    working on cards

---
+++ click-game/src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+   return ( 
+    <div class="jumbotron">
+    <h1 class="display-4">Rick and Morty Click Game</h1>
+    <p class="lead">Let's get into the action!</p>
+    <hr class="my-4"></hr>
+    </div>
+        
+    )
+}
+
+export default Jumbotron;
---
+++ click-game/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"></a>
+    </nav>
+    );
+}
+
+export default Nav; 
