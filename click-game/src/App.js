19fa85a0378376d5f4390ad91da33c4ff8f20eb6
jenenneihart <neihart.jenen@gmail.com>
2020-07-25 16:15:55 -0700

    created the onclick to respond to user and created scores

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -1,4 +1,4 @@
-mport React, { Component } from 'react';
+import React, { Component } from 'react';
 // important components 
 import CharacterCard from "./components/Cards";
 import Wrapper from "./components/Wrapper";
@@ -13,3 +13,38 @@ class App extends Component {
     topScore: 0,
     message: "Click on Different Characters to Earn Points"
   };
+ 
+  handleClick = (id, clicked) => {
+
+    const cardOrder = this.state.cards;
+
+    if (clicked) {
+      cardOrder.forEach((card, index) => {
+        cardOrder[index].clicked = false;
+      });
+      return this.setState({
+        card: cardOrder.sort(() => Math.random() - 0.5),
+        message: "You guessed wrong!",
+        score: 0
+      })
+    }
+    else {
+      cardOrder.forEach((card, index) => {
+        if (id === card.id) {
+          cardOrder[index].clicked = true;
+        }
+      });
+
+      const { topScore, score } = this.state;
+      const newScore = score + 1;
+      const newTopScore = newScore > topScore ? newScore : topScore;
+
+      return this.setState({
+        card: cardOrder.sort(() => Math.random() - 0.5),
+        message: "You are correct!",
+        score: newScore,
+        topScore: newTopScore,
+      })
+    }
+  };
+}
